d58f11dba7779fe6531b694b1c794af2744c85f5
the-little-light <atellcolmjk55@gmail.com>
2023-07-13 19:17:34 +0800

    4 finished

--- boot/build.sh
+++ boot/build.sh
@@ -2,4 +2,5 @@
 nasm -o mbr.bin mbr.S
 dd if=./mbr.bin of=../bochs/hd60M.img bs=512 count=1 conv=notrunc 
 nasm -o loader.bin loader.S
-dd if=./loader.bin of=../bochs/hd60M.img bs=512 count=1 seek=2 conv=notrunc
+dd if=./loader.bin of=../bochs/hd60M.img bs=512 count=3 seek=2 conv=notrunc
+rm ./mbr.bin ./loader.bin
