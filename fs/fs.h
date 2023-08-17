3f8367591630e96b6b514c8bd1968a243efbd468
the-little-light <atellcolmjk55@gmail.com>
2023-08-17 13:51:27 +0800

    6

--- fs/fs.h
+++ fs/fs.h
@@ -38,4 +38,5 @@ extern struct partition* cur_part; // 默认情况下操作的是哪个分区
 void filesys_init(void);
 int32_t path_depth_cnt(char* pathname);
 int32_t sys_open(const char* pathname,uint8_t flags);
+int32_t sys_close(int32_t fd);
 #endif
